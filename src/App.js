ff55113ad245dffd1d8a7f11b280cc954ac91d6d
skimsing <114190498+skimsing@users.noreply.github.com>
2022-11-30 19:44:59 -0800

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.sass";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//components
 import Header from "./components/Header/Header";
+
+//pages
 
 function App() {
 	return (
 		<div className="App">
-			<Header />
+			<BrowserRouter>
+			
+			<Routes>
+
+			</Routes>
+			<Route element={<Header/>}/>
+			</BrowserRouter>
 		</div>
 	);
 }
