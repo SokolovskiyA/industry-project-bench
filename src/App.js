ec63213d435e02c3f7882fea7bda80d43698b54b
skimsing <114190498+skimsing@users.noreply.github.com>
2022-12-01 00:03:34 -0800

    fixed path

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 			<BrowserRouter>
 			
 			<Routes>
-			<Route to="/" element={<DashboardHeader />}/>
+			<Route path="/" element={<DashboardHeader />}/>
 
 			</Routes>
 			</BrowserRouter>
